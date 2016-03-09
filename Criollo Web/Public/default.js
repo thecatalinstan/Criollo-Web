0cfa1633ca6687bdb29ef65790fc4f96eafbfa3d
Cătălin Stan <catalin.stan@me.com>
2016-03-09 19:22:18 +0100

    Compile Jade and Stylus files and bundle them in the specified folder

--- Criollo Web/Public/default.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
