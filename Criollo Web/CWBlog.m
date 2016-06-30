3c12417bd559ab69fa2c3d1219f7c009d69ef422
Cătălin Stan <catalin.stan@me.com>
2016-06-30 15:28:12 +0200

    Import authors from JSON encoded user defaults key

--- Criollo Web/CWBlog.m
+++ Criollo Web/CWBlog.m
@@ -6,14 +6,16 @@
 //  Copyright © 2016 Criollo.io. All rights reserved.
 //
 
+#import <JSONModel/JSONModel.h>
+
 #import "CWBlog.h"
 #import "CWBlogAuthor.h"
 #import "CWAppDelegate.h"
+#import "CWUser.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CWBlog ()
-
 
 @end
 
@@ -58,22 +60,40 @@ NS_ASSUME_NONNULL_END
 
 - (void)importUsersFromDefaults:(NSError * _Nullable __autoreleasing *)error {
     [[NSUserDefaults standardUserDefaults ] synchronize];
-    NSDictionary * defaultsUsers = [[NSUserDefaults standardUserDefaults] dictionaryForKey:CWDefaultsUsersKey];
+    NSArray<NSString *> * defaultsUsers = [[NSUserDefaults standardUserDefaults] arrayForKey:CWDefaultsUsersKey];
     if ( !defaultsUsers ) {
         return;
     }
 
     [self.managedObjectContext performBlockAndWait:^{
-        [defaultsUsers enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, id _Nonnull obj, BOOL * _Nonnull stop) {
-            NSError * fetchError = nil;
-            CWBlogAuthor *author = [CWBlogAuthor fetchAuthorForUsername:key inManagedObjectContext:self.managedObjectContext error:&fetchError];
-            if ( fetchError ) {
+        [defaultsUsers enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+
+            JSONModelError * jsonModelError = nil;
+            CWUser * user = [[CWUser alloc] initWithString:obj error:&jsonModelError];
+
+            *error = nil;
+            if ( jsonModelError ) {
+                *error = jsonModelError.copy;
                 *stop = YES;
-            } else if ( !author ) {
+                return;
+            }
+
+            *error = nil;
+            CWBlogAuthor *author = [CWBlogAuthor fetchAuthorForUsername:user.username inManagedObjectContext:self.managedObjectContext error:error];
+            if ( *error ) {
+                *stop = YES;
+                return;
+            }
+
+            if ( !author ) {
                 author = [[CWBlogAuthor alloc] initWithEntity:[NSEntityDescription entityForName:NSStringFromClass([CWBlogAuthor class]) inManagedObjectContext:self.managedObjectContext] insertIntoManagedObjectContext:self.managedObjectContext];
-                author.user = key;
-            };
+            }
+
+            author.user = user.username;
+            author.email = user.email;
+            author.displayName = [[NSString stringWithFormat:@"%@ %@", user.firstName ? : @"", user.lastName ? : @""] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
         }];
+        
         *error = nil;
         [self saveManagedObjectContext:error];
     }];
