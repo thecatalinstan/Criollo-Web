0eb5036233d51a2b3dc43c72cadd1d4c09b96cf7
Cătălin Stan <catalin.stan@me.com>
2016-07-04 15:18:35 +0200

    Use api wrapper instead of external library XHR functions

--- Criollo Web/Public/app.js
+++ Criollo Web/Public/app.js
@@ -1,23 +1,18 @@
-import m from 'mithril'
-import hljs from 'highlight.js'
-import stats from './stats.js'
+import getStats from './stats.js'
 import menu from './menu.js'
+
 // import notificationCenter from './notifications.js'
 // import login from './login.js'
 
-window.m = m
 window.onload = () => {
-  // Code highlighting
-  hljs.initHighlighting()
-
-  // Setup notification center
-  // const defaultNotificationCenter = notificationCenter($(document.body))
-
   // Footer info
-  m.mount(document.getElementById('stats-info'), stats)
+  getStats(document.getElementById('stats-info'))
 
   // Menu
   menu.setup()
+
+  // Setup notification center
+  // const defaultNotificationCenter = notificationCenter($(document.body))
 
   // Login form
   // login.setup((payload) => {
--- Criollo Web/Public/stats.js
+++ Criollo Web/Public/stats.js
@@ -1,24 +1,15 @@
-import m from 'mithril'
+import api from './api.js'
 
-const getStats = (vnode) => {
-  m.request({method: "GET", url: `/api/info?${Math.random()}`}).map( (response) => {
-    vnode.state.response = response
-    window.setTimeout(() => { getStats(vnode) }, 3000)
-    m.redraw()
-  })
-}
-
-export default {
-  view: (vnode) => {
-    let response = vnode.state.response || {}
-    if ( response.data ) {
-      let memoryUsage = response.data.memoryInfo > 0 ? ` using ${response.data.memoryInfo} of memory` : ""
-      return m('p', `${response.data.processName} ${response.data.processVersion}${memoryUsage}, running for ${response.data.runningTime} on ${response.data.unameSystemVersion}. Served ${response.data.requestsServed} requests.`)
-    } else {
-      return m('p')
+const getStats = (container) => {
+  api({ url: `/api/info?${Math.random()}` }, (data) => {
+    let stats = '';
+    if (data) {
+      let memoryUsage = data.memoryInfo > 0 ? ` using ${data.memoryInfo} of memory` : ""
+      stats = `${data.processName} ${data.processVersion}${memoryUsage}, running for ${data.runningTime} on ${data.unameSystemVersion}. Served ${data.requestsServed} requests.`
     }
-  },
-  oninit: (vnode) => {
-    getStats(vnode)
-  }
+    container.innerHTML = stats
+    window.setTimeout(getStats.bind(null, container), 3000)
+  }, console.error)
 }
+
+export default getStats
