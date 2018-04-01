d2d8f4a139045760839ac4bf17d0d467a1ab5fb4
Cătălin Stan <catalin.stan@me.com>
2018-04-01 14:57:23 +0200

    Update bundled to 0.5.1

--- Criollo Web/CWAppDelegate.h
+++ Criollo Web/CWAppDelegate.h
@@ -13,7 +13,7 @@
 #define CWGitHubURL                         @"https://github.com/thecatalinstan/Criollo"
 #define CWWebGitHubURL                      @"https://github.com/thecatalinstan/Criollo-Web"
 #define CWSessionCookie                     @"cwsession"
-#define CWCriolloVersion                    @"0.5.0"
+#define CWCriolloVersion                    @"0.5.1"
 
 #define CWStaticDirPath                     @"/static"
 #define CWLoginPath                         @"/login"
